8bfae3411b0a19a8f2a2b7b6a6ce019b182ef284
ivica-k <ivica.kolenkas@gmail.com>
2022-10-22 18:26:10 +0200

    1.1 - hello world

---
+++ savealife/app.py
@@ -0,0 +1,18 @@
+from os import getenv
+from chalice import Chalice
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+first_name = getenv("WORKSHOP_NAME", "ivica")  # replace with your own name of course
+
+app = Chalice(app_name=f"{first_name}-savealife")
+
+
+@app.route("/")
+def index():
+    return {"hello": "world"}
