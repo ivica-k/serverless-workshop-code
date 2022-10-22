5b96ab8988c24b9a865159032fc33341ea1fa3a0
ivica-k <ivica.kolenkas@gmail.com>
2022-10-22 21:00:12 +0200

    4.4 - fixes donor sign up tests

--- savealife/tests/test_app.py
+++ savealife/tests/test_app.py
@@ -1,23 +1,30 @@
 import json
+import os
+from unittest import mock
 
-from app import app
 from chalice.test import Client
 
+ENV = os.getenv("ENV", "dev")
+first_name = os.getenv("WORKSHOP_NAME", "ivica")  # replace with your own name of course
 
+
+@mock.patch.dict(os.environ, {"TABLE_NAME": f"{first_name}-savealife-{ENV}"})
 def test_donor_signup():
+    from app import app
+
     json_payload = {
         "first_name": "ivica",
         "city": "Amsterdam",
         "type": "A+",
-        "email": "ivica@server.com"
+        "email": "ivica@server.com",
     }
 
     with Client(app) as client:
         response = client.http.post(
             "/donor/signup",
             headers={"Content-Type": "application/json"},
-            body=json.dumps(json_payload)
+            body=json.dumps(json_payload),
         )
 
         assert response.status_code == 200
-        assert response.json_body == json_payload
+        assert response.json_body.get("success")
