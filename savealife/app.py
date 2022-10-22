cd5548af9963412256bfb68b6d7d3a44338d6e2e
ivica-k <ivica.kolenkas@gmail.com>
2022-10-22 18:26:10 +0200

    1.5 - adds basic logging

--- savealife/app.py
+++ savealife/app.py
@@ -1,3 +1,5 @@
+import logging
+
 from os import getenv
 from chalice import Chalice
 
@@ -11,9 +13,13 @@ except ImportError:
 first_name = getenv("WORKSHOP_NAME", "ivica")  # replace with your own name of course
 
 app = Chalice(app_name=f"{first_name}-savealife")
+app.log.setLevel(logging.DEBUG)
 
 @app.route("/donor/signup", methods=["POST"])
 def donor_signup():
     body = app.current_request.json_body
+
+    app.log.debug(f"Received JSON payload: {body}")
+    app.log.info("This is a INFO level message")
 
     return body
